c5b0b378c9e06cee2b7766620931712854dbf5a5
lit-john <lit-john@users.noreply.github.com>
2024-10-14 16:30:57 +0100

    Updated Unity version

--- Assets/Scripts/RadioController.cs
+++ Assets/Scripts/RadioController.cs
@@ -6,17 +6,7 @@ public class RadioController : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource theAudioSource;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void PlayNextSong()
     {
         theAudioSource.Play();
